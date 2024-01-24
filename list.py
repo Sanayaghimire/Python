682ee74affa737e1a49d7cbfb6aeac36282b2d83
Sanayaghimire <sanaya04ghimire@gmail.com>
2024-01-24 22:56:50 +0545

    list exercise1

---
+++ list.py
@@ -0,0 +1,32 @@
+names = ['danish', 'eru','roshan','kedar','binod']
+print(names)
+print("Hey "+names[0].title() + ", I would like to invite you to dinner tonight at mine.")
+print("Hey "+names[1].title()+ ", I would like to invite you to dinner tonight at mine.")
+print("Hey "+names[2].title()+ ", I would like to invite you to dinner tonight at mine.")
+print("Hey "+names[3].title()+ ", I would like to invite you to dinner tonight at mine.")
+print("Hey "+names[-1].title()+ ", I would like to invite you to dinner tonight at mine.\n\n")
+print("Unfortunately "+names[1].title()+" may not able to make it to our plan tonight.")
+
+
+
+names[1]='Mandira'
+
+print("Hey "+names[1].title()+ ", I would like to invite you to dinner tonight at mine.\n\n")
+
+print("Hello everyone, I just found the bigger dinner table")
+names.insert(0,'Ramesh')
+names.insert(3, 'Sulav')
+names.insert(7, 'Ram')
+print(names)
+print("\n")
+print("Hey "+names[0].title() + ", I would like to invite you to dinner tonight at mine.")
+print("Hey "+names[1].title()+ ", I would like to invite you to dinner tonight at mine.")
+print("Hey "+names[2].title()+ ", I would like to invite you to dinner tonight at mine.")
+print("Hey "+names[3].title()+ ", I would like to invite you to dinner tonight at mine.")
+print("Hey "+names[4].title()+ ", I would like to invite you to dinner tonight at mine.")
+print("Hey "+names[5].title()+ ", I would like to invite you to dinner tonight at mine.")
+print("Hey "+names[6].title()+ ", I would like to invite you to dinner tonight at mine.")
+print("Hey "+names[-1].title()+ ", I would like to invite you to dinner tonight at mine.\n\n")
+
+
+
