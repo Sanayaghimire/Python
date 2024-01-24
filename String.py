704804db96b42aed584f4b21cef27db297708636
Sanayaghimire <sanaya04ghimire@gmail.com>
2024-01-24 20:39:19 +0545

    string exercise

---
+++ String.py
@@ -0,0 +1,17 @@
+name= "rani"
+print("Hello "+ name.title() + ", would you like to learn some python today?")
+
+quote='"A person who never made a mistake never tried anything new."'
+author = "Alberta Einstein"
+
+print(author +" once said, "+ quote)
+
+famous_person="Balen"
+message=famous_person + " is a son of "+author +"."
+print(message)
+
+name ="\t babu baiya \n"
+                                                                            
+print(name.rstrip())
+print(name.lstrip())
+print(name.strip())
