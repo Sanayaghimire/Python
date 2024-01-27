0a762a1cbcdaae4c62e2053a805a7a8a50042ccd
Sanayaghimire <sanaya04ghimire@gmail.com>
2024-01-27 22:51:47 +0545

    tuple exercise

---
+++ tuple.py
@@ -0,0 +1,6 @@
+buffet=('daal vaat', 'chowein','roti','momo', 'naan')
+for food in buffet:
+    print(food)
+modified_buffet=('daal vaat', 'momo','chowein', 'pizza','burger')
+for food in modified_buffet:
+    print("The modified menu is: "+food)
